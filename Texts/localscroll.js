726a1916abbf61a8a643d39823ec41e05497acee
trochr <trocher@gmail.com>
2015-05-21 16:36:56 +0200

    Update localscroll.js

--- Texts/localscroll.js
+++ Texts/localscroll.js
@@ -5,7 +5,7 @@
 var asSettings = {wordsReadPerMinute: 180,
     interval: null,
     scrolling: 1,
-    debug: true,
+    debug: false,
     curElm: null,
     debugInvokeDelay: 200,
     lastEscPressTime: 0,
